8d13cccc243d37b9ed3afc774c0524740144ff76
Sanjay Namdeo <sanjay.namdeo@live.com>
2020-06-01 10:27:37 +0200

    Added login/logout feature with local state

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -3,6 +3,8 @@ import React, {Component} from 'react';
 const API_KEY = '';
 
 class GoogleAuth extends Component {
+    state = {isSignedIn: null};
+
     componentDidMount() {
         window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
@@ -10,17 +12,41 @@ class GoogleAuth extends Component {
                 'scope': 'email'
             }).then(() => {
                 this.auth = window.gapi.auth2.getAuthInstance();
-                console.log(this.auth);
+                this.setState({isSignedIn: this.auth.isSignedIn.get()});
+                this.auth.isSignedIn.listen(this.handleAuthChange);
             })
         })
-
     }
+
+    handleAuthChange = (isSignedIn) => {
+        this.setState({isSignedIn});
+    }
+
+    renderLoginButton = (isSignedIn) => {
+        if (!isSignedIn) {
+            return (
+                <div className='ui google button red' onClick={this.onSignInClick}>
+                    <i className='google icon'/>
+                    Sign In with Google
+                </div>
+            );
+        } else if (isSignedIn) {
+            return (
+                <div className='ui google button red' onClick={this.onSignOutClick}>
+                    <i className='google icon'/>
+                    Sign Out
+                </div>
+            );
+        } else return null;
+    }
+
+    onSignInClick = () => this.auth.signIn();
+    onSignOutClick = () => this.auth.signOut();
 
     render() {
         return (
-            <div className='ui google button red'>
-                <i  className='google icon'/>
-                Sign In
+            <div>
+                {this.renderLoginButton(this.state.isSignedIn)}
             </div>
         );
     }
