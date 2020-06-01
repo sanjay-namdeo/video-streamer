5f7ede7f676c4a8dcaa909df3e5676aaa97b4b31
Sanjay Namdeo <sanjay.namdeo@live.com>
2020-06-01 16:25:12 +0200

    Implemented redux to manage SignIn and SignOut and manage signed in user state

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {signIn, signOut} from './actions/actions';
 
 const API_KEY = '';
 
 class GoogleAuth extends Component {
-    state = {isSignedIn: null};
-
+    // Load google client:auth2
     componentDidMount() {
         window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
@@ -12,16 +13,29 @@ class GoogleAuth extends Component {
                 'scope': 'email'
             }).then(() => {
                 this.auth = window.gapi.auth2.getAuthInstance();
-                this.setState({isSignedIn: this.auth.isSignedIn.get()});
+                this.handleAuthChange(this.auth.isSignedIn.get());
                 this.auth.isSignedIn.listen(this.handleAuthChange);
             })
         })
     }
 
+    // Attach signIn listener to invoke actions on status change
     handleAuthChange = (isSignedIn) => {
-        this.setState({isSignedIn});
+        if (isSignedIn) {
+            const userId = this.auth.currentUser.get().getId();
+            this.props.signIn(userId);
+        } else {
+            this.props.signOut();
+        }
     }
 
+    // When user clicks on Sign In with Google
+    onSignInClick = () => this.auth.signIn();
+
+    // When user clicks on Sign Out
+    onSignOutClick = () => this.auth.signOut();
+
+    // Render button based on sign in status
     renderLoginButton = (isSignedIn) => {
         if (!isSignedIn) {
             return (
@@ -40,16 +54,17 @@ class GoogleAuth extends Component {
         } else return null;
     }
 
-    onSignInClick = () => this.auth.signIn();
-    onSignOutClick = () => this.auth.signOut();
-
     render() {
         return (
             <div>
-                {this.renderLoginButton(this.state.isSignedIn)}
+                {this.renderLoginButton(this.props.isSignedIn)}
             </div>
         );
     }
 }
 
-export default GoogleAuth;
+// Convert redux state, which will be injected as props in this component
+const makeStateToProps = (state) => ({isSignedIn: state.auth.isSignedIn});
+
+// connect to redux and get actions
+export default connect(makeStateToProps, {signIn, signOut})(GoogleAuth);
--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
+import {Provider} from 'react-redux';
+import {createStore} from "redux";
+import reducers from "./components/reducers";
+
 ReactDOM.render(
-    <App/>,
+    <Provider store={createStore(reducers)}>
+        <App/>
+    </Provider>,
     document.querySelector('#root')
 );
