1744d6b853b848c2086713e9be091c7068960b71
Sanjay Namdeo <sanjay.namdeo@live.com>
2020-06-01 09:42:31 +0200

    Added Google Auth to load gapi and get auth object

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,6 +1,21 @@
 import React, {Component} from 'react';
 
+const API_KEY = '';
+
 class GoogleAuth extends Component {
+    componentDidMount() {
+        window.gapi.load('client:auth2', () => {
+            window.gapi.client.init({
+                'clientId': API_KEY,
+                'scope': 'email'
+            }).then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance();
+                console.log(this.auth);
+            })
+        })
+
+    }
+
     render() {
         return (
             <div className='ui google button red'>
