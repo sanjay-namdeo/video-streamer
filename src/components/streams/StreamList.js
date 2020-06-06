d673da2d2e45daa8ba206eefba9071622a8e6198
Sanjay Namdeo <sanjay.namdeo@live.com>
2020-06-06 10:37:52 +0200

    Implemented stream list component

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,11 +1,39 @@
 import * as React from 'react';
+import {connect} from 'react-redux';
+import {fetchStreams as fetchStreamsAction} from './../actions/actions';
 
-const StreamList = () => {
-    return (
-        <div>
-            StreamList
-        </div>
-    );
-};
+class StreamList extends React.Component {
+    renderList = () => {
+        const {streams} = this.props;
 
-export default StreamList;
+        return Object.keys(streams).map((key) => {
+            return (
+                <div className='item' key={key}>
+                    <i className='large forward middle aligned icon red'/>
+                    <div className='content'>
+                        <div className="header"><h4>{streams[key].title}</h4></div>
+                        <div className="description">{streams[key].description}</div>
+                    </div>
+                </div>
+            )
+        });
+    }
+
+    componentDidMount() {
+        this.props.fetchStreamsAction();
+    }
+
+    render() {
+        return (
+            <div className='ui relaxed divided list container'>{this.renderList()}</div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        streams: state.streams
+    }
+}
+
+export default connect(mapStateToProps, {fetchStreamsAction})(StreamList);
