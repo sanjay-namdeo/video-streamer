a1944b0a603336e75913f138613cc34869576893
Sanjay Namdeo <sanjay.namdeo@live.com>
2020-06-06 10:11:51 +0200

    Added reducers form edit, delete and fetch stream(s)

--- src/components/reducers/index.js
+++ src/components/reducers/index.js
@@ -1,6 +1,15 @@
-import {SIGN_IN, SIGN_OUT, STREAM_CREATE} from "../actions/types";
+import {
+    FETCH_STREAM,
+    FETCH_STREAMS,
+    SIGN_IN,
+    SIGN_OUT,
+    STREAM_CREATE,
+    STREAM_DELETE,
+    STREAM_EDIT
+} from "../actions/types";
 import {combineReducers} from "redux";
 import {reducer as formReducer} from 'redux-form';
+import _ from 'lodash';
 
 const INITIAL_STATE = {
     isSignedIn: null,
@@ -22,6 +31,14 @@ const streamReducer = (state = {}, action) => {
     switch (action.type) {
         case STREAM_CREATE:
             return {...state, [action.payload.id]: action.payload};
+        case STREAM_EDIT:
+            return {...state, [action.payload.id]: action.payload};
+        case STREAM_DELETE:
+            return _.omit(state, action.payload.id);
+        case FETCH_STREAM:
+            return state[action.payload];
+        case FETCH_STREAMS:
+            return {...state, ...action.payload};
         default:
             return state;
     }
